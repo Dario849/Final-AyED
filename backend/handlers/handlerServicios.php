6a4034a3f8136f61f98af76f0df7d83fa2c8ce85
Dario Rodriguez <dariorodriguez7810@gmail.com>
2025-11-23 21:04:42 -0300

    Panel añadir trabajos a servicio ya existente

--- backend/handlers/handlerServicios.php
+++ backend/handlers/handlerServicios.php
@@ -13,13 +13,13 @@ class HandlerServicios
         $action = explode('_', $action);
         switch ($action[1]) {
             case 'list'://Trae tabla completa de servicios, sin parametros
-                return $this->listar($data);
+                return $this->listServices($data);
             case 'trabajos'://Trae relación completa entre un servicio, y sus trabajos, trae información completa del servicio, cliente, y matriz completa de trabajos realizados (se espera id de servicio como parametro).
-                return $this->servicioTrabajos($data);
+                return $this->listJobsOfService($data);
             case 'nuevo'://Realizar insersión de nuevo servicio a cliente + vehículo pre-seleccionados (se inicia con monto en 0, estado = Recibido, con 0 trabajos realizados)
 
             case 'agregar'://Realizar insersión a servicio existente, un nuevo trabajo, su monto, fecha, y insertar en consecuencia a tabla servicio el monto total actualizado, fecha estado + estado = en proceso
-
+                return $this->addJobToService($data);
             case 'modificar'://Realizar cambios al estado del servicio, como estado terminado no entregado y entregado, modifica fecha estado a su vez.
 
             default:
@@ -27,7 +27,7 @@ class HandlerServicios
         }
 
     }
-    private function listar($data)
+    private function listServices($data)
     {
         try {
             $sql = "SELECT s.*, c.apellido_nombre,c.DNI,v.patente,m.nombre FROM servicios AS s LEFT JOIN vehiculos AS v ON v.id = s.id_vehiculo LEFT JOIN clientes AS c ON c.id = v.id_cliente LEFT JOIN modelos AS m ON m.id = v.id_modelo";
@@ -47,7 +47,7 @@ class HandlerServicios
             echo json_encode(['status' => 'error', 'message' => 'Error al buscar el producto: ' . $e->getMessage()]);
         }
     }
-    private function servicioTrabajos($data)
+    private function listJobsOfService($data)
     {
         try {
             $sql = "SELECT 
@@ -56,7 +56,9 @@ class HandlerServicios
             s.importe AS s_importe,
             s.estado AS s_estado,
             s.fecha_estado AS s_fecha_estado,
+            v.id AS v_id,
             v.patente AS v_patente,
+            c.id AS c_id,
             c.apellido_nombre,
             c.DNI,
             c.te_linea,
@@ -89,6 +91,33 @@ class HandlerServicios
                 echo json_encode(['status' => 'error', 'message' => 'Producto no encontrado.']);
             }
 
+        } catch (PDOException $e) {
+            http_response_code(500);
+            echo json_encode(['status' => 'error', 'message' => 'Error al buscar el producto: ' . $e->getMessage()]);
+        }
+    }
+    private function addJobToService($data)
+    {
+        try {
+            $total = 0;
+            $this->pdo->beginTransaction();
+            $sql = "SELECT importe FROM trabajos WHERE id = ?";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([$data['jobId']]);
+            $jobImporte = $stmt->fetchColumn();
+            $total = $data['jobCount'] * $jobImporte;
+
+            $sql = "INSERT INTO servicios_det (id_servicio, id_trabajo, id_vehiculo, cantidad, importe) VALUES (?, ?, ?, ?, ?)";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([$data['serviceId'], $data['jobId'], $data['vehicleId'], $data['jobCount'], $jobImporte]);
+
+            $sql = "UPDATE servicios SET importe = importe + ?, estado = '1', fecha_estado = NOW() WHERE id = ?";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([$total, $data['serviceId']]);
+
+            $this->pdo->commit();
+
+            echo json_encode(['status' => 'success', 'message' => 'Trabajo agregado al servicio correctamente.']);
         } catch (PDOException $e) {
             http_response_code(500);
             echo json_encode(['status' => 'error', 'message' => 'Error al buscar el producto: ' . $e->getMessage()]);
